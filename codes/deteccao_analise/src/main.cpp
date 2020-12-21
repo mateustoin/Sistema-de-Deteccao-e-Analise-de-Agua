f90346c0a0cc0f44aa828dfdfd1d0c880f8d6677
Mateus Antonio <mateustoin@gmail.com>
2020-12-21 15:16:06 -0300

    feat: Rotina de Voltametria ciclica básica concluída

--- codes/deteccao_analise/src/main.cpp
+++ codes/deteccao_analise/src/main.cpp
@@ -42,12 +42,15 @@ void startVoltammetry(int time_interval, float min_voltage, float max_voltage){
 	// Determina os valores de duty cycle para atender a tensão de variação de saída requerida
 	int min_duty_cycle = map(min_voltage, 0.0, 3.3, 0.0, 255.0);
 	int max_duty_cycle = map(max_voltage, 0.0, 3.3, 0.0, 255.0);
-	
+
+	int voltageValue = 0;
+	double currentValue = 0.0;
+
+	/*
 	// Cria contadores para determinar o fim da coleta de dados
 	int current_counter = 0;
 	int counter_limit = max_duty_cycle - min_duty_cycle;
 
-	/*
 	Lógica do millis pode ser utilizada para criar uma função não bloqueante futuramente
 
 	int init_time = millis();
@@ -58,15 +61,26 @@ void startVoltammetry(int time_interval, float min_voltage, float max_voltage){
 	for (int i = min_duty_cycle; i <= max_duty_cycle; i++){
 		ledcWrite(pwm_channel, i);
 		delay(intervalCollect);
-		// coleta dados
-		
+		// Coleta dados de corrente e tensão de acordo com tabela verdade da corrente
+		voltageValue = analogRead(pinReadSignal);
+		if (voltageValue > 511){
+			currentValue = mapfloat(voltageValue, 512, 1023, 0.0, 0.0002);
+		}else{
+			currentValue = (-1)*mapfloat(voltageValue, 0, 511, 0.0, 0.0002);
+		}
 	}
 
 	// Realiza variação de tensão negativamente na saída e coleta dados
 	for (int i = max_duty_cycle; i >= min_duty_cycle; i--){
 		ledcWrite(pwm_channel, i);
 		delay(intervalCollect);
-		// coleta dados
+		// Coleta dados de corrente e tensão de acordo com tabela verdade da corrente
+		voltageValue = analogRead(pinReadSignal);
+		if (voltageValue > 511){
+			currentValue = mapfloat(voltageValue, 512, 1023, 0.0, 0.0002);
+		}else{
+			currentValue = (-1)*mapfloat(voltageValue, 0, 511, 0.0, 0.0002);
+		}
 	}
 
 }
